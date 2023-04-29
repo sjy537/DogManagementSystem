48cc144c32ac547570aa8f8aacce3e7a12e803dc
sjy537 <111789986+sjy537@users.noreply.github.com>
2023-04-29 18:16:17 -0500

    Add files via upload

---
+++ src/main/java/com/sanjay/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package com.sanjay.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* @author Sanjay Kumar Pilli
+*
+*/
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
