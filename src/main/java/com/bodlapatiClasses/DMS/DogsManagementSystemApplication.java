b5242259ff159090676d624c47353a76388b2833
sjy537 <111789986+sjy537@users.noreply.github.com>
2023-04-29 20:56:05 -0500

    changes

---
+++ src/main/java/com/bodlapatiClasses/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.bodlapatiClasses.DMS;
+/**
+* @author Sowmya Reddy Bodlapati
+*
+*/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bodlapatiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,12 @@
+package com.bodlapatiClasses.DMS.repository;
+/**
+* @author Sowmya Reddy Bodlapati
+*
+*/
+import org.springframework.data.repository.CrudRepository;
+
+import com.bodlapatiClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
